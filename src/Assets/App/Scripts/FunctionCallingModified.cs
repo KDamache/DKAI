6ff5545d6648c6685a6826add046040ede38aabf
Ali Mahmoud <ali.mahmoud2607@gmail.com>
2025-11-22 22:01:29 +0100

    liaison entre le modèle STT et le LLM + functionCalling

---
+++ src/Assets/App/Scripts/FunctionCallingModified.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using LLMUnity;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace LLMUnitySamples
+{
+    public static class FunctionsModified
+    {
+        public static string Talk()
+        {
+            return "JE PARLE";
+        }
+
+        public static string Attack()
+        {
+            return "JE LUI TIRE DESSUS AVEC MON BAZOUZOU3000";
+        }
+
+        public static string Heal()
+        {
+            return "J'ENVOIE UNE POTION DE SOIN";
+        }
+    }
+
+    public class FunctionCallingModified : MonoBehaviour
+    {
+        public LLMCharacter llmCharacter;
+
+        void Start()
+        {
+            llmCharacter.grammarString = MultipleChoiceGrammar();
+        }
+
+        string[] GetFunctionNames()
+        {
+            List<string> functionNames = new List<string>();
+            foreach (var function in typeof(FunctionsModified).GetMethods(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly)) functionNames.Add(function.Name);
+            return functionNames.ToArray();
+        }
+
+        string MultipleChoiceGrammar()
+        {
+            return "root ::= (\"" + string.Join("\" | \"", GetFunctionNames()) + "\")";
+        }
+
+        string ConstructPrompt(string message)
+        {
+            string prompt = "Which of the following choices matches best the input?\n\n";
+            prompt += "Input:" + message + "\n\n";
+            prompt += "Choices:\n";
+            foreach(string functionName in GetFunctionNames()) prompt += $"- {functionName}\n";
+            prompt += "\nAnswer directly with the choice";
+            return prompt;
+        }
+
+        string CallFunction(string functionName)
+        {
+            return (string) typeof(FunctionsModified).GetMethod(functionName).Invoke(null, null);
+        }
+
+        public async void OnAudioTranscripted(string message)
+        {
+            string functionName = await llmCharacter.Chat(ConstructPrompt(message));
+            string result = CallFunction(functionName);
+            Debug.Log($"Calling {functionName}\n{result}");
+        }
+
+        public void CancelRequests()
+        {
+            llmCharacter.CancelRequests();
+        }
+
+        bool onValidateWarning = true;
+        void OnValidate()
+        {
+            if (onValidateWarning && !llmCharacter.remote && llmCharacter.llm != null && llmCharacter.llm.model == "")
+            {
+                Debug.LogWarning($"Please select a model in the {llmCharacter.llm.gameObject.name} GameObject!");
+                onValidateWarning = false;
+            }
+        }
+    }
+}
